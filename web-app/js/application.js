ef3afd7d6d8c3a1d9563bd825bb169c195b00a39
Chris Anatalio <anataliocs@gmail.com>
2014-12-16 00:57:55 -0500

    code cleanup

--- web-app/js/application.js
+++ web-app/js/application.js
@@ -1,8 +1,8 @@
 if (typeof jQuery !== 'undefined') {
-    (function($) {
-        $('#spinner').ajaxStart(function() {
+    (function ($) {
+        $('#spinner').ajaxStart(function () {
             $(this).fadeIn();
-        }).ajaxStop(function() {
+        }).ajaxStop(function () {
             $(this).fadeOut();
         });
     })(jQuery);
@@ -13,7 +13,7 @@ var app = angular.module('FacebookAPI', ['appControllers']);
 var appControllers = angular.module('appControllers', []);
 
 appControllers.controller('FacebookCtrl', ['$scope', '$rootScope', '$http',
-    function($scope, $rootScope, $http) {
+    function ($scope, $rootScope, $http) {
 
         window.fbAsyncInit = function () {
             FB.init({
@@ -33,97 +33,24 @@ appControllers.controller('FacebookCtrl', ['$scope', '$rootScope', '$http',
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
 
-
-
-
         $scope.FBUser = null;
 
-        $scope.signup = function() {
+        $scope.signup = function () {
             alert("TODO: Do signup with user: " + JSON.stringify($scope.user));
-        };
-
-        $scope.user = {};
-        // $scope.user.first_name=null;
-        // $scope.user.last_name=null;
-        // $scope.user.email=null;
-        // $scope.user.bio=null;
-        // $scope.user.hometown=null;
-        // $scope.user.location=null;
-        // $scope.user.gender=null;
-        // $scope.user.birthday=null;
-        // $scope.user.website=null;
-        // $scope.user.facebook_link=null;
-
-        /*
-         * Get user profile info
-         * */
-        $scope.getMe = function() {
-            FB.api('/me', function(response) {
-                console.log(response);
-                $scope.FBUser = response;
-                var user = $scope.user;
-                user.first_name = response.first_name;
-                user.last_name = response.last_name;
-                user.email = response.email;
-                user.bio = response.bio;
-                if (response.hometown != undefined) {
-                    user.hometown = response.hometown.name;
-                }
-                if (response.location != undefined) {
-                    user.location = response.location.name;
-                }
-                user.gender = response.gender;
-                user.birthday = response.birthday;
-                user.website = response.website;
-                user.facebook_link = response.link;
-                $scope.$apply();
-            });
-        };
-
-        $scope.getPermissions = function() {
-            FB.api('/me/permissions', function(response) {
-                console.log(response);
-                $scope.fbpermissions = response;
-                $scope.$apply();
-            });
-        };
-
-        /*
-         * Get profile picture from Facebook
-         * */
-        $scope.getFBPictureUrl = function(id){
-            return "https://graph.facebook.com/" + id + "/picture?type=large";
-        }
-
-        /*
-        * Get message feed from Group
-        * */
-        $scope.getGroupFeed = function() {
-            FB.api('43962888127?fields=feed', function(response) {
-
-                if (response && !response.error) {
-                    $scope.msgFeed = response.feed.data;
-                }
-
-
-            });
         };
 
         /*
          * Auth with FB and retrieve data
          * */
-        $scope.fblogin = function() {
+        $scope.fblogin = function () {
             if ($scope.FBUser == null) {
-                FB.login(function(response) {
+                FB.login(function (response) {
                     //console.log(response);
                     if (response.authResponse) {
                         //console.log('Logged in.');
                         $scope.FBAuthResponse = response.authResponse;
-                        $scope.getPermissions();
-                        $scope.getMe();
-                        //$scope.getGroupFeed();
 
-                        FB.api('43962888127?fields=feed', function(response) {
+                        FB.api('43962888127?fields=feed', function (response) {
 
                             if (response && !response.error) {
                                 $scope.msgFeed = response.feed.data;
