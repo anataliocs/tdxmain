87ea9d71f0c83fb35e0f4df38fb52f963da160b5
Chris Anatalio <anataliocs@gmail.com>
2014-12-22 21:35:05 -0500

    Create new user
    Create new user

---
+++ grails-app/controllers/com/tdx/UserController.groovy
@@ -0,0 +1,41 @@
+package com.tdx
+
+class UserController {
+
+    def index() {}
+
+    def create() {}
+
+    def edit() {}
+
+    def show() {
+        [ users : User.list() ]
+    }
+
+    def save() {
+
+        User newUser = new User()
+
+        newUser.username = params.username
+        newUser.password = params.password
+
+        newUser.enabled = true
+        newUser.accountExpired = false
+        newUser.accountLocked = false
+        newUser.passwordExpired = false
+
+        newUser.save(flush: true)
+
+        def roleUser = Role.findByAuthority('ROLE_USER')
+
+        UserRole userRole = new UserRole()
+
+        userRole.user = newUser
+        userRole.role = roleUser
+
+        userRole.save(flush: true)
+
+        redirect(action: "show")
+    }
+
+}
